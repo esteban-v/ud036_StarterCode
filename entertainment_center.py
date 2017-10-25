fcb37eded96141fba269d340aa13c5b7f3a4ef89
Esteban <esteban.v.web@gmail.com>
2017-10-25 16:10:30 -0500

    Update storyline for all movies

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,22 +2,22 @@ import fresh_tomatoes
 import media
 
 justice_league = media.Movie("Justice League",
-                     "Justice League is an upcoming American superhero film based on the DC Comics superhero team of the same name, distributed by Warner Bros. Pictures",
+                     "Justice League is an upcoming American superhero film based on the DC Comics superhero team of the same name, distributed by Warner Bros. Pictures.",
                      "https://upload.wikimedia.org/wikipedia/en/3/31/Justice_League_film_poster.jpg",
                      "https://www.youtube.com/watch?v=RWCQ22JyCL4")
 
 guardians_of_the_galaxy = media.Movie("Guardians of the Galaxy",
-                     "Guardians of the Galaxy is a 2014 American superhero film based on the Marvel Comics superhero team of the same name, produced by Marvel Studios and distributed by Walt Disney Studios Motion Pictures",
+                     "Guardians of the Galaxy is a 2014 American superhero film based on the Marvel Comics superhero team of the same name, produced by Marvel Studios.",
                      "https://upload.wikimedia.org/wikipedia/en/8/8f/GOTG-poster.jpg",
                      "https://www.youtube.com/watch?v=d96cjJhvlMA")
 
 batman_begins = media.Movie("Batman Begins",
-                     "Batman Begins is a 2005 superhero film based on the DC Comics character Batman, co-written and directed by Christopher Nolan and starring Christian Bale, Michael Caine, Liam Neeson, Katie Holmes, Gary Oldman, Cillian Murphy, Tom Wilkinson, Rutger Hauer, Ken Watanabe and Morgan Freeman",
+                     "Batman Begins is a 2005 superhero film based on the DC Comics character Batman, co-written and directed by Christopher Nolan.",
                      "https://upload.wikimedia.org/wikipedia/en/a/af/Batman_Begins_Poster.jpg",
                      "https://www.youtube.com/watch?v=neY2xVmOfUM")
 
 tron = media.Movie("Tron: Legacy",
-                     "Tron: Legacy is a 2010 American science fiction action film directed by Joseph Kosinski from a screenplay written by Adam Horowitz and Edward Kitsis, based on a story by Horowitz, Kitsis, Brian Klugman and Lee Sternthal",
+                     "Tron: Legacy is a 2010 American science fiction action film directed by Joseph Kosinski from a screenplay written by Adam Horowitz and Edward Kitsis.",
                      "https://upload.wikimedia.org/wikipedia/en/c/c2/Tron_Legacy_poster.jpg",
                      "https://www.youtube.com/watch?v=L9szn1QQfas")
 
