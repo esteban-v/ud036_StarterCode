86e6037febf63b01ba5aa131afc042e40c0b8981
Esteban <esteban.v.web@gmail.com>
2017-10-27 18:48:06 -0500

    Update media.py to meet code style guidelines

--- media.py
+++ media.py
@@ -1,13 +1,21 @@
 import webbrowser
 
+
 class Movie():
-    VALID_RATINGS = ["G","PG","PG-13","R"]
-    
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    """ This class creates instances of movies that will contain
+        instance variables for each movie's title, storyline,
+        poster_image_url and trailer_youtube_url """
+
+    def __init__(self, movie_title, movie_storyline,
+                 poster_image, trailer_youtube):
+        """ This constructor initializes the required instance variables """
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
         self.trailer_youtube_url = trailer_youtube
 
     def show_trailer(self):
+        """ This method opens the web browser with the url
+            provided by the instance variable trailer_youtube_url
+            to display the movie trailer """
         webbrowser.open(self.trailer_youtube_url)
